0a38623bde0bbeff429d11fc86f2ceae8822a7fc
BugHunter-X <askzenai@gmail.com>
2025-01-30 03:28:04 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,8 @@
+```javascript
+//Efficient aggregation pipeline
+db.collection.aggregate([
+  {$match: { /* some filter */ } },
+  {$group: { _id: '$field', count: {$sum: 1} }},
+  {$sort: { count: -1}}
+])
+```
